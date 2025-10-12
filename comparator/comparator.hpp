d8f7a95e359385609132b459c79138f102a69887
Alexey <lavrik-aleksey-1@yandex.ru>
2025-10-12 12:45:08 +0300

    class comparator, measuring time

---
+++ comparator/comparator.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <set>
+
+#include "tree.hpp"
+#include "commands.hpp"
+#include "range_query.hpp"
+
+template <typename KeyT>
+class Comparator {
+public:
+    std::vector<KeyT> data;
+    size_t number_of_queries = 0;
+
+    std::vector<KeyT> answers_of_my_tree;
+    long long time_of_my_tree;
+
+    std::vector<KeyT> answers_of_std_tree;
+    long long time_of_std_tree;
+
+    void ReadData(std::istream& stream) {
+        for (char cmd = 0; stream >> cmd;) {
+            data.push_back(cmd);
+            if (cmd == Commands::kKey) {
+                KeyT key;
+                stream >> key;
+                data.push_back(key);
+            } else if (cmd == Commands::kQuery) {
+                ++number_of_queries;
+                KeyT fst;
+                KeyT snd;
+                stream >> fst >> snd;
+                data.push_back(fst);
+                data.push_back(snd);
+            } else {
+                throw std::runtime_error("Unknown command: " + cmd);
+            }
+        }
+    }
+
+    void Test(Trees::RBTree<KeyT>& tree) {
+        answers_of_my_tree.reserve(number_of_queries);
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (size_t i = 0; i != data.size(); ++i) {
+            if (data[i] == Commands::kKey) {
+                tree.Insert(data[++i]);
+            } else if (data[i] == Commands::kQuery) {
+                KeyT first = data[++i];
+                KeyT second = data[++i];
+                answers_of_my_tree.push_back(range_query(tree, first, second));
+            } else {
+                throw std::runtime_error("Unknown command: " + data[i]);
+            }
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        time_of_my_tree = duration.count();
+    }
+
+    void Test(std::set<KeyT>& tree) {
+        answers_of_std_tree.reserve(number_of_queries);
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (size_t i = 0; i != data.size(); ++i) {
+            if (data[i] == Commands::kKey) {
+                tree.insert(data[++i]);
+            } else if (data[i] == Commands::kQuery) {
+                KeyT first = data[++i];
+                KeyT second = data[++i];
+                answers_of_std_tree.push_back(range_query(tree, first, second));
+            } else {
+                throw std::runtime_error("Unknown command: " + data[i]);
+            }
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        time_of_std_tree = duration.count();
+    }
+};
---
+++ comparator/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+#include "comparator.hpp"
+
+using KeyT = int;
+
+int main() {
+    Comparator<KeyT> c;
+
+    c.ReadData(std::cin);
+
+    Trees::RBTree<KeyT> my_tree;
+    c.Test(my_tree);
+
+    std::set<KeyT> std_tree;
+    c.Test(std_tree);
+
+    std::cout << "Trees::RBTree Time: " << c.time_of_my_tree << " microseconds\n";
+    std::cout << "std::set Time: " << c.time_of_std_tree << " microseconds\n";
+
+    return 0;
+}
