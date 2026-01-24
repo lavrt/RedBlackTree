dd1f9bd7794bcd526f980a8a05d165adec979683
Alexey <lavrik-aleksey-1@yandex.ru>
2026-01-24 22:12:18 +0300

    minor fixes in comparator

--- comparator/main.cpp
+++ comparator/main.cpp
@@ -10,14 +10,15 @@ int main() {
 
     c.ReadData(std::cin);
 
-    Trees::RBTree<KeyT> my_tree;
-    c.Test(my_tree);
-
     std::set<KeyT> std_tree;
     c.Test(std_tree);
 
-    std::cout << "Trees::RBTree Time: " << c.time_of_my_tree << " microseconds\n";
     std::cout << "std::set Time: " << c.time_of_std_tree << " microseconds\n";
+
+    Trees::RBTree<KeyT> my_tree;
+    c.Test(my_tree);
+
+    std::cout << "Trees::RBTree Time: " << c.time_of_my_tree << " microseconds\n";
 
     return 0;
 }
